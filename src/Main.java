e9f85b28b2d5b58d7c1a9b1ccf1bba3dce6bd131
nb556 <nicklas.bouchard@gmail.com>
2022-10-26 13:31:42 +0300

    4

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import ibcsutils.ReadViaURL;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String addr = "http://www-personal.umich.edu/~jlawler/wordlist";
+        String[] words = ReadViaURL.readWords(addr, true, true, true);
+
+        String SearchFor = "abc";
+
+        for (String i : words)
+        {
+            if (i.equals(SearchFor))
+            {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
