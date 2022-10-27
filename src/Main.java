32def522714c5a665ebf40d17fff0c8ad095ebdc
nb556 <nicklas.bouchard@gmail.com>
2022-10-27 10:11:04 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main
 
         String SearchFor = "abc";
 
+        /*
         for (String i : words)
         {
             if (i.equals(SearchFor))
@@ -16,6 +17,24 @@ public class Main
                 System.out.println("found");
                 break;
             }
+        }
+        */
+        boolean found = false;
+        int lo = 0, hi = words.length -1;
+
+        while (!found && lo <= hi)
+        {
+            int mid = (lo+hi)/2;
+            if (words[mid].equals(SearchFor))
+            {
+                found = true;
+                System.out.println("found " + mid);
+            }
+            else if (SearchFor.compareTo(words[mid]) < 0)
+                hi = mid -1;
+            else
+                lo = mid + 1;
+
         }
     }
 }
